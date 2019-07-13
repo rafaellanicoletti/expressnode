b99b2a4fe504526af6eccab588986fe3417d0423
rafaella nicoletti <rafaella_np@hotmail.com>
2019-07-13 12:37:43 -0400

    server

--- server.js
+++ server.js
@@ -35,5 +35,34 @@ app.get("/reserve", function(req,res){
 
 });
 
+// Display Characters
+app.get("/api/tables", function(req,res){
+    return res.json(tables);
+});
+
+app.get("/api/waitlist", function(req,res){
+    return res.json(waitList);
+
+});
 
 
+//Create characters - takes in JSON input
+
+app.post("/api/tables", function(req,res){
+    var newTable = req.body;
+
+    console.log(newTable);
+
+    if (tables.length < 5) { 
+        tables.push(newTable);
+        res.json(true);
+    } else { 
+        waitList.push(newTable);
+        res.json(false);
+    }
+});
+
+//Start the server to begin listening 
+app.listen(PORT, function(){
+    console.log("app listening on PORT" + PORT);
+});
